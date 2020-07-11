13f9c3cf65953c01043c790507d3e22265d640ad
Jitesh <ijitesh.jha@gmail.com>
2020-07-11 14:46:30 +0530

    Added Java doc and Updated char count example

--- hackerranks-problems/src/test/fizz/java8/FizBuzzImpl.java
+++ hackerranks-problems/src/test/fizz/java8/FizBuzzImpl.java
@@ -23,8 +23,16 @@ public class FizBuzzImpl {
     static FizzBuzz defaultValue = (number) -> Optional.of(String.valueOf(number));
     static FizzBuzz fizz = (number) -> dividorFunction.apply(number, 3, "Fizz");
     static FizzBuzz buzz = (number) -> dividorFunction.apply(number, 5, "Buzz");
+    static FizzBuzz seven = (number) -> dividorFunction.apply(number, 7, "Seven");
+    static FizzBuzz Four = (number) -> dividorFunction.apply(number, 4, "Four");
 
-    private static String fizzBuzzNumbers(Integer number, List<FizzBuzz> suppliers) {
+    /**
+     * Steps:
+     * Taking instances of FizzBuzz to apply on integer. Iterating over it and returning String
+     * Once all the FizzBuzz applied then collecting the returned String
+     * If result of intermediate operation is empty then returning the default value as String value of Integer.
+     */
+    private String fizzBuzzNumbers(Integer number, List<FizzBuzz> suppliers) {
         return Optional.of(suppliers.stream()
                 .map(supplier -> supplier.divideReturn(number))
                 .filter(Optional::isPresent)
@@ -35,12 +43,15 @@ public class FizBuzzImpl {
 
     public static void main(String[] args) {
 
+        FizBuzzImpl impl = new FizBuzzImpl();
         List<FizzBuzz> fiz = new LinkedList<>();
         fiz.add(fizz);
         fiz.add(buzz);
+        fiz.add(Four);
+        fiz.add(seven);
 
-        for (int i = 1; i < 16; i++) {
-            String returns = fizzBuzzNumbers(i, fiz);
+        for (int i = 1; i < 50; i++) {
+            String returns = impl.fizzBuzzNumbers(i, fiz);
             System.out.println("int " + i + "= " + returns);
         }
     }
