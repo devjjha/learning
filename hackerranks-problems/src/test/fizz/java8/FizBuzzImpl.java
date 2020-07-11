7e13f73db79cf9d8bda3ec536c9cb11bbec0e864
Jitesh <ijitesh.jha@gmail.com>
2020-07-11 15:43:39 +0530

    Added parallel methods

--- hackerranks-problems/src/test/fizz/java8/FizBuzzImpl.java
+++ hackerranks-problems/src/test/fizz/java8/FizBuzzImpl.java
@@ -5,6 +5,8 @@ import java.util.List;
 import java.util.Optional;
 import java.util.function.BiPredicate;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 interface DividerFunction<N, D, R, S> {
     S apply(N n, D d, R r);
@@ -32,13 +34,40 @@ public class FizBuzzImpl {
      * Once all the FizzBuzz applied then collecting the returned String
      * If result of intermediate operation is empty then returning the default value as String value of Integer.
      */
-    private String fizzBuzzNumbers(Integer number, List<FizzBuzz> suppliers) {
+    private String fizzBuzzNumbersLinear(Integer number, List<FizzBuzz> suppliers) {
         return Optional.of(suppliers.stream()
                 .map(supplier -> supplier.divideReturn(number))
                 .filter(Optional::isPresent)
                 .map(Optional::get)
                 .collect(Collectors.joining())
         ).map(val -> val.isEmpty() ? defaultValue.divideReturn(number).get() : val).get();
+    }
+
+    private String fizzBuzzNumbersParallel(Integer number, List<FizzBuzz> suppliers) {
+        Stream<FizzBuzz> stream = suppliers.stream().parallel();
+        System.out.println(stream.isParallel());
+        return Optional.of(stream
+                .map(supplier -> supplier.divideReturn(number))
+                .filter(Optional::isPresent)
+                .map(Optional::get)
+                .collect(Collectors.joining())
+        ).map(val -> val.isEmpty() ? defaultValue.divideReturn(number).get() : val).get();
+    }
+
+    private long doLinearOperation(int range, List<FizzBuzz> fiz) {
+        long fizzBuzzNumbersST = System.currentTimeMillis();
+        IntStream.range(1, range).forEach(i -> System.out.println(fizzBuzzNumbersLinear(i, fiz)));
+        long fizzBuzzNumbersET = System.currentTimeMillis();
+
+        return (fizzBuzzNumbersET - fizzBuzzNumbersST);
+    }
+
+    private long doParallelOperation(int range, List<FizzBuzz> fiz) {
+        long fizzBuzzNumbersST = System.currentTimeMillis();
+        IntStream.range(1, range).forEach(i -> System.out.println(fizzBuzzNumbersParallel(i, fiz)));
+        long fizzBuzzNumbersET = System.currentTimeMillis();
+
+        return (fizzBuzzNumbersET - fizzBuzzNumbersST);
     }
 
     public static void main(String[] args) {
@@ -49,10 +78,11 @@ public class FizBuzzImpl {
         fiz.add(buzz);
         fiz.add(Four);
         fiz.add(seven);
+        int range = 5000;
+        long linerTime = impl.doLinearOperation(range, fiz);
+        long parallelTime = impl.doParallelOperation(range, fiz);
+        System.out.println("Linear Time elapse in millis >> " + linerTime);
+        System.out.println("Parallel Time elapse in millis >> " + parallelTime);
 
-        for (int i = 1; i < 50; i++) {
-            String returns = impl.fizzBuzzNumbers(i, fiz);
-            System.out.println("int " + i + "= " + returns);
-        }
     }
 }
